897f9a15eaae1a7c2b9f879e423b9608c2a6cf13
Lovino Maxime <maximelovino@gmail.com>
2017-08-29 14:18:10 +0200

    progress on README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# DemocraticChess
+# Democratic Chess
+
+# What is it?
+
+It's a game of multiplayer chess, where each team is composed of multiple players that vote on the next move to make.
+
+# How is it build?
+
+We're running the game engine server-side, written in JavaScript, and all communications are made using websockets. The client will draw the game using Canvas.
+
+# Who will work on what?
+ - Maxime Lovino will work on the client-server communication, parsing of the messages and interaction with the game engine and client.
+ - Vincent Tournier will write the game engine with help from Thomas Ibanez.
+ - Thomas Ibanez will take care of the frontend.
--- server/index.js
+++ server/index.js
@@ -2,14 +2,52 @@ const webSocket = require('ws');
 
 const server = new webSocket.Server({port: 8080});
 
-var client = undefined;
+var players = [];
+
+
+function player(pseudo,team) {
+
+}
+
+function pickRandomTeam() {
+
+}
+
 
 function parseMessage(message) {
+    /*
+    Syntax for messages:
+
+    Client -> Server:
+
+    Move vote: MOVE_<StartCell><EndCell> (can receive more than one per people, so we count the last one)
+    Show available cells: CLICK_<Cell>
+    First message after connection : pseudo
+
+    Server -> client:
+
+    Team assignment: TEAM_<white/black>
+    Move done: MOVED_<StartCell><EndCell>
+    Result: RESULT_<white/black>
+    Response to available cells: AVAILABLE_<listOfCells>
+    Timeout (or all votes done): TIMEOUT_<teamToPlay>
+    Board (send all board at the current state): BOARD_<Array>
+     */
+
+
+
+
+
+
+
+
     console.log(`${client}: ${message}`);
 }
 
 
 server.on('connection', function connection(ws, req) {
+    //On connection, will join team and add to player list (assign team)
+    //On disconnect, remove from team
     client = req.connection.remoteAddress;
     ws.on('message', parseMessage);
     console.log(`New connection from ${client}`);
