4c9195e9b2b53ee62a7bf161a4f02cd29edca939
Osoldier <thomas.ibanez33@gmail.com>
2017-08-30 14:38:58 +0200

    Basic message sending working

--- client/scripts/chessclient.js
+++ client/scripts/chessclient.js
@@ -3,7 +3,7 @@
 const SERVER_URL = "ws://172.20.0.208:8080";
 let team = -1;
 let connected = false;
-let id = -1;
+let id = 0;
 let socket;
 
 function onMessageReceived(msg) {
@@ -15,15 +15,16 @@ function connect(name) {
     socket.onmessage = onMessageReceived;
     socket.onerror = function(e) {
         alert("An error occured, you are not connected");
+        return;
     };
 }
 
-function handleMessage(e, id) {
+function handleMessage(e, tagId) {
     if(e.which == 13 || e.keyCode == 13) {
-        let msg = document.getElementById(id).value;
+        let msg = document.getElementById(tagId).value;
         if(msg !== "") {
-            socket.send();
-            document.getElementById(id).value = "";
+            socket.send(communication(id, newMessage(messageType.CHAT, msg)));
+            document.getElementById(tagId).value = "";
         }
     }
 }
--- client/scripts/chessrender.js
+++ client/scripts/chessrender.js
@@ -5,6 +5,9 @@ const color2 = "#b58863";
 const textColor = "#777777";
 const tileSize = 83;
 
+let ctx;
+
+
 function renderPieces(ctx, board) {
 
 }
@@ -19,9 +22,9 @@ function renderBoard(ctx) {
             ctx.fillStyle = color2;
         }
         ctx.fillRect(x*tileSize, y*tileSize, tileSize, tileSize);
-        x++;
+        x += 1;
         if(x >= 8) {
-            y++;
+            y += 1;
             ctx.fillStyle = textColor;
             ctx.fillText(""+y,x*tileSize+2, y*tileSize-tileSize/2+10);
             x = 0;
@@ -31,13 +34,13 @@ function renderBoard(ctx) {
     while(x < 8) {
         ctx.fillStyle = textColor;
         ctx.fillText(String.fromCharCode(65+x), x*tileSize+tileSize/2-10, 8*tileSize+17);
-        x++;
+        x += 1;
     }
 }
 
 function init() {
     let canvas = document.getElementById("chessboard");
-    let ctx = canvas.getContext("2d");
+    ctx = canvas.getContext("2d");
     ctx.font = "20px Arial";
     renderBoard(ctx);
     let name = "";
